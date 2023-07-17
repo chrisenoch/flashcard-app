4c31e11ce2d4943d31ec26f19776278b0813503a
chrisenoch <christopher.enoch@yahoo.co.uk>
2023-07-17 10:59:14 +0200

    tailwind colours added to global CSS variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
-  },
-  plugins: [],
+    plugins: [],
+  }
 }
-
