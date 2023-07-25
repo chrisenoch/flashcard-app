52a6655a2d83f347750765f9e3e3f3586719b85b
chrisenoch <christopher.enoch@yahoo.co.uk>
2023-07-25 21:20:20 +0200

    basic accordion functionality working

--- src/app/custom-components/accordion/accordion-tab/accordion-tab.component.ts
+++ src/app/custom-components/accordion/accordion-tab/accordion-tab.component.ts
@@ -7,24 +7,10 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class AccordionTabComponent {
   @Input() tabId = ''; //make it so shjows error if not provided
-  //get map of active states from parent component
-  @Input() tabIds: Map<string, boolean> = new Map();
-
-  //set isActive state here depending on value of map and tabId
-  //isActive = this.tabIds.get(this.tabId); // this is derived from map, which is received from parent component
-
   isActive = false;
 
-  // ngOnInit() {
-  // }
-
-  @Output() updateIsActive = new EventEmitter<{
-    tabId: string;
-    isActive: boolean;
-  }>();
-
   toggleTab() {
-    console.log('in toggle tab11111');
-    this.updateIsActive.emit({ tabId: this.tabId, isActive: !this.isActive });
+    this.isActive = !this.isActive;
+    console.log('in toggle in accordion-tab component');
   }
 }
--- src/app/custom-components/accordion/accordion.component.ts
+++ src/app/custom-components/accordion/accordion.component.ts
@@ -1,11 +1,13 @@
 import {
   AfterContentChecked,
+  AfterContentInit,
   Component,
   ContentChild,
   ContentChildren,
   QueryList,
 } from '@angular/core';
 import { AccordionTabComponent } from './accordion-tab/accordion-tab.component';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-accordion',
@@ -15,18 +17,20 @@ import { AccordionTabComponent } from './accordion-tab/accordion-tab.component';
 export class AccordionComponent implements AfterContentChecked {
   tabIds: Map<string, boolean> = new Map(); //To do: change boolean type to object later
 
-  //receive click event from child and then update child
-
-  // @ViewChildren(ChildDirective) viewChildren!: QueryList<ChildDirective>;
-
   // Query for a VIEW child of type `ChildViewComponent`
   @ContentChildren(AccordionTabComponent)
   contentChildren!: QueryList<AccordionTabComponent>;
 
   ngAfterContentChecked() {
+    const newTabIds: Map<string, boolean> = new Map();
+
     this.contentChildren.forEach((ele) => {
-      console.log(ele.isActive);
-      console.log(ele.tabId);
+      newTabIds.set(ele.tabId, ele.isActive);
+      // console.log(ele.isActive);
+      // console.log(ele.tabId);
     });
+
+    this.tabIds = newTabIds;
+    console.log(this.tabIds);
   }
 }
