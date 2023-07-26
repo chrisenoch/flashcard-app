83825dabb96659ed2f027ae95271316b241022d3
chrisenoch <christopher.enoch@yahoo.co.uk>
2023-07-26 11:24:58 +0200

    user can now decide if multiple accordion tabs can be active at once with the multiple property

--- src/app/custom-components/accordion/accordion.component.ts
+++ src/app/custom-components/accordion/accordion.component.ts
@@ -1,9 +1,8 @@
 import {
   AfterContentChecked,
-  AfterContentInit,
   Component,
-  ContentChild,
   ContentChildren,
+  Input,
   QueryList,
 } from '@angular/core';
 import { AccordionTabComponent } from './accordion-tab/accordion-tab.component';
@@ -15,22 +14,62 @@ import { fromEvent } from 'rxjs';
   styleUrls: ['./accordion.component.scss'],
 })
 export class AccordionComponent implements AfterContentChecked {
+  @Input() multiple = true;
+  //tabIds map could be useful for easily deciding how to style the tabs
   tabIds: Map<string, boolean> = new Map(); //To do: change boolean type to object later
+  activeTabId: string | null = null; //keeps track of the activeTabId
+  tabIdToRemove: string | null = null;
 
-  // Query for a VIEW child of type `ChildViewComponent`
+  //use this to get the isActive status of the accordion tabs
   @ContentChildren(AccordionTabComponent)
   contentChildren!: QueryList<AccordionTabComponent>;
 
   ngAfterContentChecked() {
-    const newTabIds: Map<string, boolean> = new Map();
+    this.ensureOnlyOneTabIsActive();
+  }
 
-    this.contentChildren.forEach((ele) => {
-      newTabIds.set(ele.tabId, ele.isActive);
-      // console.log(ele.isActive);
-      // console.log(ele.tabId);
-    });
+  private ensureOnlyOneTabIsActive() {
+    if (!this.multiple) {
+      for (let i = 0; i < this.contentChildren.length; i++) {
+        let ele = this.contentChildren.get(i);
 
-    this.tabIds = newTabIds;
-    console.log(this.tabIds);
+        if (ele) {
+          //update map with tabIds.
+          this.tabIds.set(ele.tabId, ele.isActive);
+
+          //no previous activeTab so just update it
+          if (ele.isActive && this.activeTabId === null) {
+            this.activeTabId = ele.tabId;
+            //need
+          } else if (
+            ele.isActive &&
+            this.activeTabId !== null &&
+            ele.tabId !== this.activeTabId
+          ) {
+            //set isActive on other tabId to false
+            this.tabIds.set(this.activeTabId, false);
+
+            //need to set it on the actual value as well
+            this.tabIdToRemove = this.activeTabId;
+
+            //update activeTabId
+            this.activeTabId = ele.tabId;
+            break;
+          }
+        }
+      }
+
+      if (this.tabIdToRemove) {
+        console.log('in remove if');
+        this.contentChildren.forEach((ele) => {
+          if (ele.tabId === this.tabIdToRemove) {
+            ele.isActive = false;
+          }
+        });
+        this.tabIdToRemove = null;
+      }
+
+      console.log(this.tabIds);
+    }
   }
 }
