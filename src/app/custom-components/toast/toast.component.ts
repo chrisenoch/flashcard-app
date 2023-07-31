0312de1d0926e4844f01a24932ccf89e0632df96
chrisenoch <christopher.enoch@yahoo.co.uk>
2023-07-31 18:27:02 +0200

    calculations added for positioning toast - the toast still does not adhere to them though

--- src/app/custom-components/toast/toast.component.ts
+++ src/app/custom-components/toast/toast.component.ts
@@ -21,18 +21,45 @@ import {
   styleUrls: ['./toast.component.scss'],
 })
 export class ToastComponent
-  implements AfterViewChecked, AfterContentChecked, AfterContentInit
+  implements AfterContentInit, AfterViewInit, AfterViewChecked
 {
   constructor(
     @Inject(DOCUMENT) document: Document,
     private renderer2: Renderer2
   ) {}
 
+  @Input() position: 'LEFT' | 'RIGHT' | 'TOP' | 'BOTTOM' = 'RIGHT';
+  @Input() gapInPx: number | undefined;
+
   @ViewChild('toast') toastViewChild!: ElementRef;
   @ContentChild('accept') acceptContentChild: ElementRef | undefined;
 
   toastViewChildCopy!: ElementRef;
+  toastHeight!: number;
+  toastWidth!: number;
   count = 0;
+  toastParentDomRect!: DOMRect;
+  top: number | null = null;
+  bottom: number | null = null;
+  left: number | null = null;
+  right: number | null = null;
+
+  ngAfterViewInit(): void {
+    this.toastHeight = this.toastViewChild.nativeElement.height;
+    this.toastWidth = this.toastViewChild.nativeElement.width;
+    this.defineCoords();
+
+    this.toastViewChildCopy = this.toastViewChild;
+    this.toastViewChild.nativeElement.parentElement.remove();
+    this.renderer2.appendChild(
+      document.body,
+      this.toastViewChildCopy.nativeElement
+    );
+  }
+
+  ngAfterViewChecked(): void {
+    //
+  }
 
   ngAfterContentInit(): void {
     if (this.acceptContentChild) {
@@ -46,27 +73,42 @@ export class ToastComponent
     }
   }
 
-  ngAfterContentChecked(): void {
-    console.log('in aftercontentchecked');
-  }
+  defineCoords() {
+    this.toastParentDomRect =
+      this.toastViewChild.nativeElement.parentElement.parentElement.getBoundingClientRect();
 
-  //change this to AfterViewInit
-  ngAfterViewChecked(): void {
-    if (this.count === 0) {
-      console.log(this.toastViewChild?.nativeElement);
-
-      setTimeout(() => {
-        setTimeout(() => {
-          console.log('toastViewChildCopy ');
-          console.log(this.toastViewChildCopy);
-          document.body.appendChild(this.toastViewChildCopy?.nativeElement);
-        }, 4000);
-        // this.toastViewChild.nativeElement.parentElement.style.display = 'block';
-        this.toastViewChildCopy = this.toastViewChild;
-        this.toastViewChild?.nativeElement.parentElement.remove();
-      }, 2000);
+    if (this.gapInPx === undefined || this.gapInPx === null) {
+      this.gapInPx = 8;
     }
-    this.count++;
+
+    switch (this.position) {
+      case 'LEFT':
+        this.left =
+          this.toastParentDomRect.left - this.toastWidth - this.gapInPx;
+        this.top = this.toastParentDomRect.height / 2 - this.toastHeight / 2;
+        break;
+      case 'RIGHT':
+        this.left =
+          this.toastParentDomRect.left +
+          this.toastParentDomRect.width +
+          this.gapInPx;
+        this.top = this.toastParentDomRect.height / 2 - this.toastHeight / 2;
+        break;
+      case 'TOP':
+        this.left = this.toastParentDomRect.width / 2 + this.toastWidth / 2;
+        this.top = this.toastHeight - this.gapInPx;
+        break;
+      case 'BOTTOM':
+        this.left = this.toastParentDomRect.width / 2 + this.toastWidth / 2;
+        this.top =
+          this.toastParentDomRect.top +
+          this.toastParentDomRect.height +
+          this.gapInPx;
+        break;
+      default:
+        const exhaustiveCheck: never = this.position;
+        throw new Error(exhaustiveCheck);
+    }
   }
 
   accept() {
