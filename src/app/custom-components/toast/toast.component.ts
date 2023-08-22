4fa9d8c36428930a734f3a2153402db38a8f2a30
chrisenoch <christopher.enoch@yahoo.co.uk>
2023-08-22 20:36:41 +0200

    compareDOMRectValues# extracted to utilities

--- src/app/custom-components/toast/toast.component.ts
+++ src/app/custom-components/toast/toast.component.ts
@@ -39,6 +39,7 @@ import { ToastService } from './toast.service';
 import { Position } from './models/position';
 import { ArrowPosition, Arrows } from './models/arrows';
 import { ControlledError } from '../errors/ControlledError';
+import { compareDOMRectValues } from '../utilities';
 
 @Component({
   selector: 'app-toast',
@@ -486,43 +487,12 @@ export class ToastComponent
     }
   }
 
-  private compareDOMRectValues(DOMRectFirst: DOMRect, DOMRectSecond: DOMRect) {
-    //DOMRect properties are not enumerable so we cannot loop over them with "for in" or Object.entries#. Also,
-    //getOwnpropertyNames# does not work because the DOMRect properties are inherited from DOMRectReadOnly https://developer.mozilla.org/en-US/docs/Web/API/DOMRect
-    const { x, y, width, height, top, right, bottom, left } = DOMRectFirst;
-    const {
-      x: x2,
-      y: y2,
-      width: width2,
-      height: height2,
-      top: top2,
-      right: right2,
-      bottom: bottom2,
-      left: left2,
-    } = DOMRectSecond;
-
-    if (
-      x !== x2 ||
-      y !== y2 ||
-      width !== width2 ||
-      height !== height2 ||
-      top !== top2 ||
-      right !== right2 ||
-      bottom !== bottom2 ||
-      left !== left2
-    ) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-
   private updateToastIfToastAnchorDomRectChanged() {
     if (this.toastAnchorDomRect) {
       const newToastAnchorDomRect =
         this.currentToastAnchor.getBoundingClientRect() as DOMRect;
 
-      const domRectsAreEqual = this.compareDOMRectValues(
+      const domRectsAreEqual = compareDOMRectValues(
         this.toastAnchorDomRect,
         newToastAnchorDomRect
       );
@@ -543,7 +513,7 @@ export class ToastComponent
       const newToastDestinationDomRect =
         this.toastDestination.getBoundingClientRect();
 
-      const domRectsAreEqual = this.compareDOMRectValues(
+      const domRectsAreEqual = compareDOMRectValues(
         this.toastDestinationDomRect,
         newToastDestinationDomRect
       );
